8dbe9a7affe626aff97b5a24b388e51b70927791
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2025-08-16 12:17:03 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -22,7 +22,7 @@ int main() {
     int arr[] = {0, 1, 2, 0, 1, 2, 1, 0};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    sort012(arr, n);
+    sortarrat012(arr, n);
 
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
