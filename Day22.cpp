683eb2ccabded2ee871ec5e2112467ff51508937
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2025-09-05 10:36:36 +0530

    Create Day22.cpp

---
+++ Day22.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstElementToRepeatKTimes(vector<int>& arr, int k) {
+    unordered_map<int, int> freq;
+
+    for (int num : arr) {
+        freq[num]++;
+    }
+
+    for (int num : arr) {
+        if (freq[num] == k) {
+            return num;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {3, 1, 4, 4, 5, 2, 6, 1, 4};
+    int k = 2;
+
+    int result = firstElementToRepeatKTimes(arr, k);
+    cout << result << endl; 
+
+    return 0;
+}
