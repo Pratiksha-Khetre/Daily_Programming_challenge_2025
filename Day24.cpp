18e15e85b89b3d9e038fd1efd0541b25bc9d56ae
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2025-09-07 22:13:07 +0530

    Create Day24.cpp

---
+++ Day24.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right) return root; 
+    return left ? left : right;     
+}
+
+TreeNode* buildTree(vector<string>& nodes) {
+    if (nodes.empty() || nodes[0] == "null") return nullptr;
+
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (i < nodes.size() && nodes[i] != "null") {
+            curr->left = new TreeNode(stoi(nodes[i]));
+            q.push(curr->left);
+        }
+        i++;
+
+        if (i < nodes.size() && nodes[i] != "null") {
+            curr->right = new TreeNode(stoi(nodes[i]));
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+TreeNode* findNode(TreeNode* root, int val) {
+    if (!root) return nullptr;
+    if (root->val == val) return root;
+    TreeNode* left = findNode(root->left, val);
+    if (left) return left;
+    return findNode(root->right, val);
+}
+
+int main() {
+    vector<string> nodes = {"3","5","1","6","2","0","8","null","null","7","4"};
+    int pVal = 5, qVal = 1;
+
+    TreeNode* root = buildTree(nodes);
+    TreeNode* p = findNode(root, pVal);
+    TreeNode* q = findNode(root, qVal);
+
+    TreeNode* lca = lowestCommonAncestor(root, p, q);
+    if (lca) cout << "LCA of " << pVal << " and " << qVal << " is: " << lca->val << endl;
+    else cout << "No LCA found." << endl;
+
+    return 0;
+}
