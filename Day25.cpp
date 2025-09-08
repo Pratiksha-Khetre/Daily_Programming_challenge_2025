df82024d41277125eb032c6b492fb9857ceaa700
Pratiksha-Khetre <pratikshakhetre1011@gmail.com>
2025-09-08 17:33:10 +0530

    Create Day25.cpp

---
+++ Day25.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return validate(root, LONG_MIN, LONG_MAX);
+    }
+
+private:
+    bool validate(TreeNode* node, long minVal, long maxVal) {
+        if (!node) return true;
+
+        if (node->val <= minVal || node->val >= maxVal)
+            return false;
+
+        return validate(node->left, minVal, node->val) &&
+               validate(node->right, node->val, maxVal);
+    }
+};
+
+int main() {
+    
+    TreeNode* root = new TreeNode(2);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(3);
+
+    Solution solution;
+    if (solution.isValidBST(root))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+
+    return 0;
+}
